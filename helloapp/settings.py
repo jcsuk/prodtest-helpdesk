6f3a65153d63ec294752daa8ef7cd54650f804ee
jcsuk <45880281+jcsuk@users.noreply.github.com>
2019-01-02 13:25:16 -0700

    Update settings.py

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 SITE_ID = 2
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,10 +122,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -134,3 +138,10 @@ EMAIL_HOST_USER = 'justcsuk@gmail.com'
 EMAIL_HOST_PASSWORD = 'Cambles21'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'justcsuk@gmail.com'
+<<<<<<< HEAD
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
