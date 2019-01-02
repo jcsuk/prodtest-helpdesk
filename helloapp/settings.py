a3dd83e3326a0cb80daa63e1918944972bdfe76b
jcsuk <45880281+jcsuk@users.noreply.github.com>
2019-01-02 12:30:51 -0700

    Update settings.py

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 SITE_ID = 2
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
